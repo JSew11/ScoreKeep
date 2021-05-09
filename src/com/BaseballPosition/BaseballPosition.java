c66f946cc2f4f151386fddb62da161c34648c7d1
JSew11 <jseward@zagmail.gonzaga.edu>
2021-05-08 21:55:04 -0700

    Updated interface to be public

--- src/com/BaseballPosition/BaseballPosition.java
+++ src/com/BaseballPosition/BaseballPosition.java
@@ -7,12 +7,12 @@ package com.BaseballPosition;
  * @version - 1.0.0
  * @since - May 8, 2021
  */
-interface BaseballPosition extends Comparable<BaseballPosition> {
+public interface BaseballPosition extends Comparable<BaseballPosition> {
     /**
      * Method that gives the numerical representation of
      * the baseball position
      *
      * @return - numerical representation of the position
      */
-    public Integer posNumber();
+    Integer posNumber();
 }
