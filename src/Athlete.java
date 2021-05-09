84dd0bcf8b8ffc132661ce596e25336e2a0d898a
JSew11 <jseward@zagmail.gonzaga.edu>
2021-05-08 22:27:56 -0700

    Created BaseballPlayer and Athlete classes

---
+++ src/Athlete.java
@@ -0,0 +1,75 @@
+/**
+ * A class for an athlete
+ *
+ * @author - Joshua Seward
+ * @version - 1.0.0
+ * @since - May 8, 2021
+ */
+public class Athlete {
+    private String first_name;
+    private String last_name;
+    private Integer number;
+
+    /**
+     * Constructor for a basic Athlete object
+     */
+    public Athlete() {
+        first_name = "";
+        last_name = "";
+    }
+
+    /**
+     * Overloaded constructor for a specific Athlete object
+     *
+     * @param first_name - athlete's first name
+     * @param last_name - athlete's last name
+     * @param number - athlete's jersey number
+     */
+    public Athlete(String first_name, String last_name, int number) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.number = Integer.valueOf(number);
+    }
+
+    /**
+     * Accessor for the athlete's first name
+     *
+     * @return - the athlete's first name
+     */
+    public String getFirst_name() {return first_name;}
+
+    /**
+     * Mutator to set the athlete's first name
+     *
+     * @param first_name - the athlete's first name
+     */
+    public void setFirst_name(String first_name) {this.first_name = first_name;}
+
+    /**
+     * Accessor for the athlete's last name
+     *
+     * @return - the athlete's last name
+     */
+    public String getLast_name() {return last_name;}
+
+    /**
+     * Mutator to set the athlete's last name
+     *
+     * @param last_name - the athlete's last name
+     */
+    public void setLast_name(String last_name) {this.last_name = last_name;}
+
+    /**
+     * Accessor for the athlete's jersey number
+     *
+     * @return - the athlete's jersey number
+     */
+    public Integer getNumber() {return number;}
+
+    /**
+     * Mutator to set the athlete's jersey number
+     *
+     * @param number - the athlete's jersey number
+     */
+    public void setNumber(int number) {this.number = Integer.valueOf(number);}
+}
---
+++ src/BaseballPlayer.java
@@ -0,0 +1,78 @@
+import com.BaseballPosition.*;
+import java.util.ArrayList;
+
+/**
+ * A class for a baseball player
+ *
+ * @author - Joshua Seward
+ * @version - 1.0.0
+ * @since - May 8, 2021
+ */
+public class BaseballPlayer extends Athlete {
+    private BaseballPosition primary_position;
+    private ArrayList<BaseballPosition> secondary_positions;
+
+    /**
+     * Constructor for a basic BaseballPlayer object
+     */
+    public BaseballPlayer() {
+        super();
+    }
+
+    /**
+     * Overloaded constructor for a specific BaseballPlayer object
+     *
+     * @param first_name - player's first name
+     * @param last_name  - player's last name
+     * @param number     - player's jersey number
+     * @param position   - player's primary position
+     */
+    public BaseballPlayer(String first_name, String last_name, int number, BaseballPosition position) {
+        super(first_name, last_name, number);
+        primary_position = position;
+    }
+
+    /**
+     * Accessor for the baseball player's primary position
+     *
+     * @return - baseball player's primary position
+     */
+    public BaseballPosition getPrimary_position() {return primary_position;}
+
+    /**
+     * Mutator for the baseball player's primary position
+     *
+     * @param primary_position - baseball player's primary position
+     */
+    public void setPrimary_position(BaseballPosition primary_position) {this.primary_position = primary_position;}
+
+    /**
+     * Accessor for the baseball player's secondary positions
+     *
+     * @return - baseball player's secondary positions
+     */
+    public ArrayList<BaseballPosition> getSecondary_positions() {return secondary_positions;}
+
+    /**
+     * Method to add a new secondary position to the ArrayList of
+     * BaseballPositions
+     *
+     * @param position - BaseballPosition to add to the ArrayList
+     */
+    public void addSecondary_position(BaseballPosition position) {
+        if(!secondary_positions.contains(position) && position.compareTo(primary_position) == 0)
+            secondary_positions.add(position);
+    }
+
+    /**
+     * Method to remove a secondary position from the ArrayList of
+     * BaseballPositions
+     *
+     * @param position - BaseballPosition to remove from the ArrayList
+     */
+    public void removeSecondary_position(BaseballPosition position) {
+        for (int i = 0; i < secondary_positions.size(); ++i)
+            if (position.compareTo(secondary_positions.get(i)) == 0)
+                secondary_positions.remove(i);
+    }
+}
