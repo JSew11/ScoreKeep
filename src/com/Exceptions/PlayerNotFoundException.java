4eef9dc9999f125a4064f3eee4fad91dbb48ee81
JSew11 <jseward@zagmail.gonzaga.edu>
2021-05-19 23:26:57 -0700

    Wrote a new Excpetion for a PlayerNotFound case in the BaseballTeam class

---
+++ src/com/Exceptions/PlayerNotFoundException.java
@@ -0,0 +1,21 @@
+package com.Exceptions;
+
+/**
+ * An exception for when a desired BaseballPlayer object
+ * does not exist
+ *
+ * @author - Joshua Seward
+ * @version - 1.0.0
+ * @since - May 19, 2021
+ */
+public class PlayerNotFoundException extends Exception {
+    /**
+     * Constructor for a PlayerNotFoundException
+     *
+     * @param errorMessage - error message for the new
+     *                       PlayerNotFoundException
+     */
+    public PlayerNotFoundException(String errorMessage) {
+        super(errorMessage);
+    }
+}
