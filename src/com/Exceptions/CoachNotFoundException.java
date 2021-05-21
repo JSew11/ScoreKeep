17f704943fdcf2d38e6d5750fcb363386965fdbe
JSew11 <jseward@zagmail.gonzaga.edu>
2021-05-21 14:01:58 -0700

    Updated javadoc commenting

--- src/com/Exceptions/CoachNotFoundException.java
+++ src/com/Exceptions/CoachNotFoundException.java
@@ -1,5 +1,19 @@
 package com.Exceptions;
 
+/**
+ * An exception for when the desired BaseballCoach object
+ * does not exist
+ *
+ * @author - Joshua Seward
+ * @version - 1.0.0
+ * @since - May 21, 2021
+ */
 public class CoachNotFoundException extends Exception {
+    /**
+     * Constructor for a CoachNotFoundException
+     *
+     * @param errorMessage - error message for the new
+     *                       CoachNotFoundException
+     */
     public CoachNotFoundException(String errorMessage) {super(errorMessage);}
 }
